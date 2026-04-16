d3542bfd1da6acfe36760219dcb141158771eb63
Maxi Rojas <arnaldomrojas@outlook.com>
2026-04-16 02:07:12 +0200

    datatypes

---
+++ Basic/Private/02-datatypes.js
@@ -0,0 +1,10 @@
+//strig
+let name = "Maxi";
+let username = 'max';
+let email = `max@maxi.com`;
+//number
+let age = 30;
+let height = 1.75;
+//boolean
+let isStudent = true;   
+let isTeacher = false;
